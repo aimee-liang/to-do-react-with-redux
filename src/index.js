a978856f116bbe422a1363da91d6d83f35737767
aimee-liang <aimeeeliang@gmail.com>
2022-05-04 17:27:48 -0400

    Updated reducers, imported Provider, store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore } from 'redux';
+import { combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+const reducer = combineReducers({
+  tasks: (state = [], action) => {
+    let newState = Object.assign({}, state)
+    switch (action.type) {
+      case "ADD":
+        newState = state.push([action.payload])
+        // return newState
+      break;
+      case "REMOVE":
+
+      break;
+      default: 
+        return state
+    }
+    return newState
+  }
+})
+
+const store = legacy_createStore(reducer, [])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
