56160f71ff272967b8060e51603b75606ac03e94
aimee-liang <aimeeeliang@gmail.com>
2022-05-05 16:23:07 -0400

    Moved reducer and store to App level

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { useState } from 'react'
+import { legacy_createStore } from 'redux';
+import { combineReducers } from 'redux';
 
 function App() {
+
+  const reducer = combineReducers({
+    tasks: (state = [], action) => {
+      let newState = Object.assign([], state)
+      switch (action.type) {
+        case "ADD":
+          newState.push(action.payload)
+        break;
+        case "REMOVE":
+          newState = newState.filter((task) => task !== action.payload)
+        break;
+        default: 
+          return state
+      }
+      return newState
+    }
+  })
+  
+  const store = legacy_createStore(reducer)
+  const state = store.getState()
+  const [task, setTask] = useState("")
+
+  const updateTask = e => {
+    setTask(e.target.value)
+  }
+  
+  const displayTasks = () => {
+    state.tasks.forEach((task) => {
+      return <li>{task}</li>
+    })
+  }
+
+  const dispatchAndUpdate = () => {
+    store.dispatch({ type: "ADD", payload: task})
+    displayTasks()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>To Do List</h2>
+      <div>
+        <input type="text" value={task} onChange={updateTask}></input>
+        <button onClick={dispatchAndUpdate}>ADD</button>
+      </div>
+    </>
   );
 }
 
